744228b17befd01b1efd6246cb0096d55e753a57
Flore Perche <flore.perche@gmail.com>
2022-03-16 12:09:38 +0100

    Booking CRUD Create

--- index.js
+++ index.js
@@ -34,8 +34,24 @@ const Booking = mongoose.model("Booking", {
   booking_info: {
     booking_number: Number,
     booking_date: String,
+    booking_start: String,
+    booking_return: String,
     booking_duration: Number,
-    booking_totalprice: Number,
+    agency_id: String,
+    agency_name: String,
+    car_id: String,
+    car_shirt_name: String,
+    car_long_name: String,
+    car_picture: String,
+    car_driver_min_age: Number,
+    car_price_details: {
+      car_day_price: Number,
+      car_location_price: Number,
+      car_included_charges: Array,
+      car_extra_fees: Array,
+      car_selected_additional_charges: Array,
+    },
+    booking_total_price: Number,
   },
 });
 
@@ -92,6 +108,64 @@ app.get("/cardetails", async (req, res) => {
     res.status(400).json(error.message);
   }
 });
+
+// Booking CRUD
+
+// Create
+app.post("/booking/create", async (req, res) => {
+  try {
+    const newBooking = new Booking({
+      client_info: {
+        client_civility: req.fields.civility,
+        client_society: req.fields.society,
+        client_firstname: req.fields.firstname,
+        client_lastname: req.fields.lastname,
+        client_email: req.fields.email,
+        client_phonenumber: req.fields.phonenumber,
+        client_birthday: req.fields.birthday,
+        client_adress: {
+          client_street: req.fields.street,
+          client_postalcode: req.fields.postalcode,
+          client_city: req.fields.city,
+          client_country: req.fields.country,
+        },
+      },
+      booking_info: {
+        booking_number: req.fields.booking_number,
+        booking_date: req.fields.booking_date,
+        booking_start: req.fields.booking_start,
+        booking_return: req.fields.booking_return,
+        booking_duration: req.fields.booking_duration,
+        agency_id: req.fields.agency_id,
+        agency_name: req.fields.agency_name,
+        car_id: req.fields.car_id,
+        car_shirt_name: req.fields.car_short_name,
+        car_long_name: req.fields.car_long_name,
+        car_picture: req.fields.car_picture,
+        car_driver_min_age: req.fields.driver_min_age,
+        car_price_details: {
+          car_day_price: req.fields.day_price,
+          car_location_price: req.fields.location_price,
+          car_included_charges: req.fields.included_charges,
+          car_extra_fees: req.fields.extra_fees,
+          car_selected_additional_charges: req.fields.additional_charges,
+        },
+        booking_total_price: req.fields.total_price,
+      },
+    });
+    await newBooking.save();
+    console.log(newBooking);
+    res.json({ message: "It worked !" });
+  } catch (error) {
+    res.status(400).json(error.message);
+  }
+});
+
+// Read
+
+// Update
+
+// Delete
 
 // Rajouter All routes
 
